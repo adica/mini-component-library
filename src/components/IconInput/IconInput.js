c10426f4fce7d80a34003168a0a940defbc092a0
adica <adi.carmel@climacell.co>
2021-12-31 12:50:08 +0200

    feat(select): add icon input component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const sizes = {
+  small: '12px',
+  large: '18px',
+};
+
+const padding = {
+  small: '20px',
+  large: '25px',
+};
+
+const heights = {
+  small: '24px',
+  large: '32px',
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,54 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  return <Wrapper style={{ width }} height={heights[size]}>
+      <IconWrapper>
+        <Icon id={icon} size={sizes[size]} strokeWidth={1} />
+      </IconWrapper>
+      <Input placeholder={placeholder} value={label} padding={padding[size]} />
+  </Wrapper>;
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  border-bottom: 1px solid ${COLORS.black};
+  height: ${p => p.height};
+
+  &:hover {
+    cursor: pointer;
+    
+    svg {
+      stroke: ${COLORS.black};
+      stroke-width: 2px;
+    }
+    
+    input {
+      color: ${COLORS.black};  
+    }
+  }
+`;
+
+const Input = styled.input`
+  padding-left: ${p => p.padding};
+  border: 0;
+  height: 100%;
+  width: 100%;
+  color: ${COLORS.gray700};
+  
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 5px;
+  top: 0;
+  height: 100%;
+  `
 
 export default IconInput;
